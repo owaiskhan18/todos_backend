baea1ab2a29c557268e128bba32d676a3ca64989
Muhammad Owais <165272983+owaiskhan18@users.noreply.github.com>
2026-01-21 16:41:06 +0500

    Refactor import statements in task.py

--- src/models/task.py
+++ src/models/task.py
@@ -1,7 +1,9 @@
-# src/models/task.py
-from typing import Optional
+# task.py
+from typing import Optional, TYPE_CHECKING
 from sqlmodel import Field, Relationship, SQLModel
-from .user import User
+
+if TYPE_CHECKING:
+    from .user import User
 
 class Task(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -9,5 +11,5 @@ class Task(SQLModel, table=True):
     description: Optional[str] = None
     completed: bool = False
 
-    owner_id: Optional[int] = Field(default=None, foreign_key="users.id")  # 👈 note "users.id"
+    owner_id: Optional[int] = Field(default=None, foreign_key="users.id")
     owner: Optional["User"] = Relationship(back_populates="tasks")
