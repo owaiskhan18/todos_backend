86d1c80eb266f21bce6cc5feaa4836296eb8b700
Muhammad Owais <165272983+owaiskhan18@users.noreply.github.com>
2026-01-21 16:28:15 +0500

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,11 +1,11 @@
+# src/models/user.py
 from typing import List, Optional
-
 from sqlmodel import Field, Relationship, SQLModel
 from pydantic import ConfigDict
-
-from ..models.task import Task
+from .task import Task
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"  # 👈 safe table name for Postgres
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     id: Optional[int] = Field(default=None, primary_key=True)
